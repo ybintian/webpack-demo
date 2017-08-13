a5947b850ed7f746d7af96dc8a1c6626006c4e7e
will yang <450431615@qq.com>
2017-08-13 21:53:22 +0800

    new page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
-
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['你好', 'webpack'], ' ');  
-  element.classList.add('hello');
-  // 将图像添加到我们现有的 div。
-  var myIcon = new Image();
-  myIcon.src = Icon;
- 
-  element.appendChild(myIcon);
-
-  console.log(Data);
+  element.innerHTML = _.join(['hello', 'webpack'], ' ');  
   return element;
 }
 
